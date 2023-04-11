eebffcfb6b0fe3bce981a197716a4ba02f48c6eb
Carlos12001 <carlos.andres12001@gmail.com>
2023-04-10 22:38:45 -0600

    feat: create client structure

---
+++ client/src/connection.c
@@ -0,0 +1,9 @@
+//
+// Created by carlo on 4/10/2023.
+//
+
+#include "connection.h"
+#include "stdio.h"
+void print_example_coneection() {
+    printf("I'm conected!!\n");
+}
---
+++ client/src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "connection.h"
+
+int main() {
+    printf("Initalize the Client\n");
+    print_example_coneection();
+    return 0;
+}
