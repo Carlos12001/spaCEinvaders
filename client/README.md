2b5592cff79c2378debbf624355df96409621170
Carlos12001 <carlos.andres12001@gmail.com>
2023-04-13 15:33:12 -0600

    build: add sdl2 to the client

---
+++ client/README.md
@@ -0,0 +1,23 @@
+# Client of spaCEinvaders
+
+Este proyecto es el cliente de spaCEinvaders, un juego de naves
+espaciales multijugador en tiempo real.
+
+Para este proyecto se utiliza sockets y SDL para la interfaz gráfica.
+
+La liberia de SDL esta la version 2.26.5, la cual se puede descargar
+
+SDL2-devel-2.26.5-mingw.zip
+https://github.com/libsdl-org/SDL/releases
+
+## Compilación
+
+Para compilar se utiliza el compilador de C++ de MinGW.
+
+Ademas de usar CMake.
+
+### Compilación con CMake
+
+Se necesita agregar como argumento la ruta de la libreria de SDL.
+
+```
--- client/src/main.c
+++ client/src/main.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
 #include "connection.h"
+#include <SDL.h>
+#include <windows.h>
 
-int main() {
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int nCmdShow){
     printf("Initalize the Client\n");
     print_example_coneection();
+
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Failed to initialize SDL! SDL_Error: %s\n",
+               SDL_GetError());
+        return -1;
+    }
+
+    // Create window
+    SDL_Window* window = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH,
+                                          WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window) {
+        printf("Failed to create window! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // Get window surface
+    SDL_Surface* surface = SDL_GetWindowSurface(window);
+    if (!surface) {
+        printf("Failed to get the surface from the window! SDL_Error: %s\n",
+               SDL_GetError());
+        return -1;
+    }
+
+    // Fill surface with white color
+    SDL_Rect rect = {0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
+    Uint32 white = SDL_MapRGB(surface->format, 255, 255, 255);
+    SDL_FillRect(surface, &rect, white);
+
+    // Update window surface
+    SDL_UpdateWindowSurface(window);
+
+    // Wait for user exit
+    SDL_Event event;
+    while (SDL_WaitEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            break;
+        }
+    }
+
+    // Clean up
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
