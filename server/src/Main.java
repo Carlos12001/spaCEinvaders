c19ba64ef4d918eb30bffc62966772bb27ac00f6
Carlos12001 <carlos.andres12001@gmail.com>
2023-04-17 11:26:16 -0600

    chore: add coments

--- server/src/Main.java
+++ server/src/Main.java
@@ -1,5 +1,6 @@
 import connections.*;
 import game.Game;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,7 +8,21 @@ public class Main {
         Game game = new Game();
         game.startGameLoop();
 
-        //
+//        Scanner scanner = new Scanner(System.in);
+//        String input = "";
+//
+//        while (!input.equals("salir")) {
+//
+//            input = scanner.nextLine();
+//
+//            if (input.equals("saludar")) {
+//                System.out.println("Hola Mundo!");
+//            }
+//        }
+//
+//        scanner.close();
+//        System.out.println("Programa terminado.");
+//
 //        Server server = new Server();
 //        server.start(25565);
     }
