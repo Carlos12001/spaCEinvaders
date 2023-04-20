1ca697fbb99914c2107f05d6e57a4a78f094fedc
Carlos12001 <carlos.andres12001@gmail.com>
2023-04-20 11:35:02 -0600

    docs: add comments

--- server/src/Main.java
+++ server/src/Main.java
@@ -2,7 +2,16 @@ import connections.*;
 import game.Game;
 import java.util.Scanner;
 
+/*
+    * Main class
+    * This class is the main class of the server
+ */
 public class Main {
+
+    /*
+     * Main method
+     * This method is the main method of the server
+     */
     public static void main(String[] args) {
         System.out.println("Hello and welcome to the Server!");
         Game game = new Game();
