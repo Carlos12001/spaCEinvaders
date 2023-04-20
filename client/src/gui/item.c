c87022bee473584641da54c713a7e6f1ec54f846
David Robles <47421546+diabloget@users.noreply.github.com>
2023-04-20 05:53:05 -0600

    Feat: image load
    Now the code is able to load icons and show them in the gui :D

---
+++ client/src/gui/item.c
@@ -0,0 +1,127 @@
+//
+// Created by Diablo on 4/19/2023.
+//
+
+#include "item.h"
+//#include "../lib/SDL2_image/x86_64-w64-mingw32/include/SDL2/SDL_image.h"
+
+
+int currentx = 5;
+int currenty = 5;
+int hCount = 0;
+
+SDL_Texture * init_image_from_file(const char* file_name, SDL_Renderer *renderer){
+    SDL_Surface *image = IMG_Load(file_name);
+    if (!image) {
+        printf("Failed to load image: %s\n", IMG_GetError());
+        // handle error here
+    }
+    SDL_Texture *image_texture = SDL_CreateTextureFromSurface(renderer, image);
+    SDL_FreeSurface(image);
+    return image_texture;
+}
+
+SDL_Texture * getImage(SDL_Renderer *renderer, int number){
+    char filename[256];
+    sprintf(filename, "./icons/%d.png", number);
+    return init_image_from_file(filename, renderer);
+}
+
+void showPicture(int number, SDL_Renderer *renderer, int x, int y){
+    SDL_Texture *image_texture = getImage(renderer, number);
+
+    SDL_Rect texture_destination;
+    texture_destination.x = x;
+    texture_destination.y = y;
+    texture_destination.w = 30;
+    texture_destination.h = 30;
+
+    // Create a source rectangle that covers the entire texture
+    SDL_Rect source_rect;
+    source_rect.x = 0;
+    source_rect.y = 0;
+    SDL_QueryTexture(image_texture, NULL, NULL, &source_rect.w, &source_rect.h);
+
+    // Copies the image into the renderer.
+    SDL_RenderCopy(renderer, image_texture, &source_rect, &texture_destination);
+
+    // Shows what was copied.
+    SDL_RenderPresent(renderer);
+
+    SDL_DestroyTexture(image_texture);
+
+}
+
+void spaceManager(){
+    if(hCount > 29){
+        hCount = 0;
+        currenty += 35;
+        currentx = 5;
+    }
+}
+
+void renderChar(char* inputChar, SDL_Renderer *renderer) {
+    char* token;
+    int i = 0;
+
+    // get the first token
+    token = strtok(inputChar, ",");
+    //int hCount = 0;
+    // process each token
+    while(token != NULL) {
+        if (i < 3) {
+            printf("%d ", atoi(token)); // print the first 3 elements as integers
+        }
+        /*
+        if(hCount > 29){
+            hCount = 0;
+            currenty += 35;
+            currentx = 5;
+        }
+         */
+
+
+        else {
+            int value = atoi(token);
+            if (value == 0) {
+                printf("vacio");
+            }
+            else if (value == 1) {
+                printf("en1 ");
+            }
+            else if (value == 2) {
+                printf("en2 ");
+            }
+            else if (value == 3) {
+                printf("en3 ");
+            }
+            else if (value == 10) {
+                printf("powerup1 ");
+            }
+            else if (value == 20) {
+                printf("powerup2 ");
+            }
+            else {
+                printf("unknown ");
+            }
+        }
+
+        // get the next token
+        token = strtok(NULL, ",");
+        currentx += 35;
+        i++;
+    }
+}
+
+
+/**
+ * SDL_Rect texture_destination;
+ *  texture_destination.x = x;
+ *  texture_destination.y = y;
+ *  texture_destination.w = w;
+ *  texture_destination.h = h;
+ * SDL_RenderClear(renderer);
+ * SDL_RenderCopy(renderer, image_texture, NULL, &texture_destination);
+ * SDL_RenderPresent(renderer);
+ */
+
--- client/src/main.c
+++ client/src/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include "connections/csocket.h"
+#include "gui/item.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <synchapi.h>
+#include "stdbool.h"
+#include "lib/SDL2_image/x86_64-w64-mingw32/include/SDL2/SDL_image.h"
 #include <SDL.h>
 #include <windows.h>
 
@@ -13,8 +16,10 @@
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                    LPSTR lpCmdLine, int nCmdShow){
 
+    char inputChar[] = "1,3,1000,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,3,3,3,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,3,3,3,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,20,20,20,0,0,0,20,20,20,0,0,0,20,20,20,0,0,0,20,20,20,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0";
+
     printf("Client Functioning\n");
-    // /**
+     /**
     int sock_fd;
     char buffer[MAX_MSG_LEN];
 
@@ -37,8 +42,41 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     }
     // **/
 
+    //SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window * win = SDL_CreateWindow("SpaceInviders", SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,1200,675, SDL_WINDOW_OPENGL);
+    SDL_Renderer *renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_Surface * screen = SDL_GetWindowSurface(win);
+    IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG);
+    //SDL_Delay(2000);
+    bool run = true;
+    int abc = 1;
+    while(run){
+        SDL_Event ev;
+        while(SDL_PollEvent(&ev))
+        {
+            switch (ev.type)
+            {
+                case SDL_QUIT:
+                    run = false;
+                    break;
+            }
+        }
+
+        SDL_RenderClear(renderer);
+        //showPicture(1, renderer, 150, 150);
+        if(abc == 1){
+            renderChar(inputChar, renderer);
+            abc = 0;
+        }
+
+    }
 
 
-
+    IMG_Quit;
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
     return EXIT_SUCCESS;
 }
