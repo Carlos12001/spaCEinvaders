e55ed40d3af4d95328ac476f66367b31311918fe
Carlos12001 <carlos.andres12001@gmail.com>
2023-04-20 23:49:25 -0600

    docs: add comments

--- server/src/connections/PlayerObserver.java
+++ server/src/connections/PlayerObserver.java
@@ -3,8 +3,23 @@ package connections;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Subject class
+ * This class is the main class of the subject
+ */
 public class PlayerObserver extends Observer{
+
+    /**
+     * This is the subject of the observer
+     */
     Integer sid;
+
+    /**
+     * This is the subject of the observer
+     * @param subject This is the subject of the observer
+     * @param server This is the server of the observer
+     * @param sid This is the sid of the observer
+     */
     public PlayerObserver(Subject subject, Server server, Integer sid){
         this.subject = subject;
         this.subject.addObserver(this);
