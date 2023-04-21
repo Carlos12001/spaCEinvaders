adab5f3c204c426df9f00fc7240a70c27301af99
Carlos12001 <carlos.andres12001@gmail.com>
2023-04-20 23:49:09 -0600

    docs: add comments

--- server/src/connections/Observer.java
+++ server/src/connections/Observer.java
@@ -1,7 +1,22 @@
 package connections;
-
+/**
+ * Observer class
+ * This class is the main class of the observer
+ */
 public abstract class Observer {
+
+    /**
+     * This is the subject of the observer
+     */
     protected Subject subject;
+
+    /**
+     * This is the server of the observer
+     */
     protected Server server;
+
+    /**
+     * This is the update method of the observer
+     */
     public abstract void update();
 }
